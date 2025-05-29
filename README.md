e2e31282ddc31862bdf2860c1ec3949546779b2f
Ravi Teja <ravitejarj25@gmail.com>
2025-05-29 12:26:36 -0500

    env fixed moved some files code changes

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ npx expo start --tunnel
 
 
 ```bash
+npm install dotenv
+npm install react-native-dotenv
 npx expo install expo-auth-session
 npm install axios
 ```
--- services/GooglePlacesService.js
+++ services/GooglePlacesService.js
@@ -1,8 +1,5 @@
-
+import { GOOGLE_MAPS_API_KEY } from '@env';
 import axios from 'axios';
-
-
-const GOOGLE_MAPS_API_KEY = "AIzaSyB-avYjudSjUQcRiwwMWRMGB_FS3-0syyQ";
 
 // Get address suggestions from Google Places
 export const getPlaceSuggestions = async (input) => {
